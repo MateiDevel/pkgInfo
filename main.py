7923f2486621788e2db1346c6c990f85359f119c
MateiDevel <serbanmatei572@gmail.com>
2025-08-20 05:56:04 -0400

    App and Window logic

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from src.ui import createLayout
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("pkgInfo")
+window.setFixedSize(400, 400)
+
+layout = createLayout()
+window.setLayout(layout)
+window.show()
+
+sys.exit(app.exec_())
