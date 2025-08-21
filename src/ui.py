30660370edfba95cfe69f080d756c70a14f61653
MateiDevel <serbanmatei572@gmail.com>
2025-08-21 11:57:10 -0400

    Absolute positioning + added a btn for choosing a pkg or file

--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,26 @@
-from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QPushButton, QLabel, QFileDialog
+from PyQt5.QtGui import QFont
 
-def createLayout():
-    layout = QVBoxLayout()
-    title = QLabel("pkgInfo")
-    layout.addWidget(title)
-    return layout
+font = QFont('Arial', 30)
+
+def createUI(window):
+    title = QLabel("pkgInfo", window)
+    title.setFont(font)
+    title.move(400 // 2 - 60, 0)
+
+    # File 
+    fileBtn = QPushButton("File", window)
+    fileBtn.move(300, 70)
+
+    def fileBtn_clicked():
+        file_path, _ = QFileDialog.getOpenFileName(
+            None,
+            "Select Package or file",
+            "",
+            "All Files(*)"
+        )
+
+        if file_path:
+            print(file_path)
+
+    fileBtn.clicked.connect(fileBtn_clicked)
