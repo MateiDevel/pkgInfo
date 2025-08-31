5dec7dc266a4a71d249b953da73702f0df3088dd
MateiDevel <serbanmatei572@gmail.com>
2025-08-31 12:30:29 +0300

    Added icon p.s. if you run on Wayland the icon wont appear

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import sys
+import os
 from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QIcon
 from src.ui import createUI
 
 app = QApplication(sys.argv)
 
+icon_path = os.path.join(os.path.dirname(__file__), "icon.png")
+print("icon:",os.path.exists(icon_path))
+
 window = QWidget()
 window.setWindowTitle("pkgInfo")
 window.setFixedSize(400, 400)
+window.setWindowIcon(QIcon(icon_path))
 
 createUI(window)
 window.show()
