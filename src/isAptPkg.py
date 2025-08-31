e10f5acbbdb72009c0047dab061d7817825e1dea
MateiDevel <serbanmatei572@gmail.com>
2025-08-31 12:49:35 +0300

    Fixed script

--- src/isAptPkg.py
+++ src/isAptPkg.py
@@ -3,4 +3,6 @@ import os
 path = None
 
 def isAptPkg():
-    os.system(f'dpkg -S {path}')
+    # the actual file it points to after following any shortcuts or links
+    real_path = os.path.realpath(path)
+    os.system(f'dpkg -S {real_path}')
