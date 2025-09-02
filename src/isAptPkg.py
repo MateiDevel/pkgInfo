f607c4d3816c1d019c6a703a2bcd087a845fe160
MateiDevel <serbanmatei572@gmail.com>
2025-09-02 17:02:26 +0300

    Using os.system() wont output the result, fixed script

--- src/isAptPkg.py
+++ src/isAptPkg.py
@@ -1,13 +1,25 @@
-import os 
+import os
+import subprocess
 
 path = None
 
-def isAptPkg():
-
+def isAptPkg(pkgStatusLb):
+    global output
     if path is None:
         print('error: path is none')
         return False
 
     # the actual file it points to after following any shortcuts or links
     real_path = os.path.realpath(path)
-    os.system(f'dpkg -S {real_path}')
+    result = subprocess.run(["dpkg", "-S", real_path], capture_output=True, text=True)
+    output = result.stdout
+    print(output)
+
+    if output:
+        pkgStatusLb.setText(f"Package found! \n {output}")
+        pkgStatusLb.adjustSize()
+    else:
+        pkgStatusLb.setText("Package not found.")
+        pkgStatusLb.adjustSize()
+    
+
