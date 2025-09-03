d91b58a4ed6bf8044a1c215006aec469e9fa9794
MateiDevel <serbanmatei572@gmail.com>
2025-09-03 13:22:37 +0300

    If path is None , change status

--- src/isAptPkg.py
+++ src/isAptPkg.py
@@ -4,9 +4,10 @@ import subprocess
 path = None
 
 def isAptPkg(pkgStatusLb):
-    global output
     if path is None:
         print('error: path is none')
+        pkgStatusLb.setText("Path is None")
+        pkgStatusLb.adjustSize()
         return False
 
     # the actual file it points to after following any shortcuts or links
@@ -16,7 +17,7 @@ def isAptPkg(pkgStatusLb):
     print(output)
 
     if output:
-        pkgStatusLb.setText(f"Package found! \n {output}")
+        pkgStatusLb.setText(f"Package found! \n{output}")
         pkgStatusLb.adjustSize()
     else:
         pkgStatusLb.setText("Package not found.")
